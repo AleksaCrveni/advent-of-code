cf0afaa4577c5a80115b713c671e7e80eb68c681
WickedyWick <crvenialeksa@gmail.com>
2023-06-17 13:35:40 +0200

    fixed updatetop3 function

--- 2022/c/1/main.c
+++ 2022/c/1/main.c
@@ -4,70 +4,73 @@
 #include <string.h>
 
 /*
-    Author Aleksa Crveni 2023
-    Time: O(n) + O(3)
+		Author Aleksa Crveni 2023
+		Time: O(n) + O(3)
 */
 
 void updateTop3(int *arr, int newValue);
 int main()
 {
-    FILE *f;
-    bool dummy = false;
-    if (dummy)
-        f = fopen("dummyInput.txt", "r");
-    else
-        f = fopen("input.txt", "r");
+	FILE *f;
+	bool dummy = false;
+	if (dummy)
+		f = fopen("dummyInput.txt", "r");
+	else
+		f = fopen("input.txt", "r");
 
-    if (f == NULL) {
-        printf("Not able to open the file!");
-    }
-    char c;
-    char *numArr = malloc(sizeof(char) * 6);
-    memset(numArr, 0, 6);
-    int sum = 0;
-    int i = 0;
-    int top3[3] = {-3,-2,-1};
-    while((c = fgetc(f)) != EOF) {
-        if (c == '\n') {
-            c = fgetc(f);
-            sum += atoi(numArr);
-            if (c == '\n' || c == EOF) {
-                updateTop3(top3, sum);
-                sum = 0;
-                memset(numArr, 0, 6);
-                i =0;
-            } else {
-                memset(numArr, 0, 6);
-                i =0;
-                numArr[i++] = c;
-            }
-            
-        } else {
-            numArr[i++] = c;
-        }
-    }
-
-    sum += atoi(numArr);
-   	updateTop3(top3, sum);
-
-    printf("TOP 1: %d calories.\n", top3[2]);
-    int totalSum = 0;
-		for (int i =0;i < 3;i++) {
-			totalSum += top3[i];
+	if (f == NULL) {
+		printf("Not able to open the file!");
+	}
+	char c;
+	char *numArr = malloc(sizeof(char) * 6);
+	memset(numArr, 0, 6);
+	int sum = 0;
+	int i = 0;
+	int top3[3] = {-3, -2, -1};
+	while ((c = fgetc(f)) != EOF) {
+		if (c == '\n') {
+			c = fgetc(f);
+			sum += atoi(numArr);
+			if (c == '\n' || c == EOF) {
+				updateTop3(top3, sum);
+				sum = 0;
+				memset(numArr, 0, 6);
+				i = 0;
+			}
+			else {
+				memset(numArr, 0, 6);
+				i = 0;
+				numArr[i++] = c;
+			}
 		}
-		printf("TOP 3 SUM: %d calories.\n", totalSum);
-    fclose(f);
+		else {
+			numArr[i++] = c;
+		}
+	}
+
+	sum += atoi(numArr);
+	updateTop3(top3, sum);
+
+	printf("TOP 1: %d calories.\n", top3[2]);
+	int totalSum = 0;
+	for (int i = 0; i < 3; i++)	{
+		totalSum += top3[i];
+	}
+	printf("TOP 3 SUM: %d calories.\n", totalSum);
+	fclose(f);
 }
 
-void updateTop3(int *arr, int newValue) {
-		if (arr[0] < newValue)
-			arr[0] = newValue;
+void updateTop3(int *arr, int newValue)
+{
+	if (arr[0] < newValue) {
+		arr[0] = newValue;
 		for (int i = 1; i <= 2; i++) {
 			if (arr[i] < newValue) {
 				arr[i - 1] = arr[i];
 				arr[i] = newValue;
-			} else
+			}
+			else
 				i = 3;
 		}
+	}
 }
-
