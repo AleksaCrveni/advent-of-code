92a016731b841ccbd51a245ff4976bb7f5d6134b
WickedyWick <crvenialeksa@gmail.com>
2023-10-28 16:58:39 +0200

    3.c

--- 2022/c/3/main.c
+++ 2022/c/3/main.c
@@ -1,2 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "../libs/hashset.h"
+
 int main() {
+  hashset_t *hash_set = init();
+  printf("%d", is_empty(hash_set));
+  return 0;
 }
+
+
