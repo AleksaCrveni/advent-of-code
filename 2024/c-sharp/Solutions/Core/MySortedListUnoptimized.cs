c30eaa1fcb46600ccd6a846a7e2af0e37fef0e2a
WickedyWick <crvenialeksa@gmail.com>
2024-12-21 15:01:00 +0100

    .net day1 p1

---
+++ 2024/c-sharp/Solutions/Core/MySortedListUnoptimized.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+  public ref struct MySortedList<T> where T : INumber<T>
+  {
+    private T[] _array;
+    // User innerArray for any iteration of the array
+    private Span<T> _innerArray;
+    // index of "last" element
+    private int _size = -1;
+    public MySortedList(int size)
+    {
+      _array = new T[size];
+      _innerArray = _array.AsSpan<T>();
+    }
+
+    public void Add(T item)
+    {
+      // Easy first and last matches
+      if (_size == -1 || _innerArray[_size] <= item)
+      {
+        _innerArray[++_size] = item;
+        return;
+      }
+
+      // First check where it should be inserted at
+      int ind = Search(ref item);
+
+      // Shift array
+      // Need to test if tis better to copy or just shift 1 by 1, prob shift?
+      Array.Copy(_array, ind, _array, ind + 1, _size++ - ind + 1);
+      _innerArray[ind] = item;
+
+    }
+
+    // returns 
+    int Search(ref T val)
+    {
+      // In my case array won't be longer than 1k records so linear seach will be good
+      for (int i = 0; i <= _size; i++)
+      {
+        if (_innerArray[i] >= val)
+          return i;
+      }
+      return _size + 1;
+    }
+
+    public int Count
+    {
+      get
+      {
+        return _size + 1;
+      }
+    }
+
+    public T this[int i]
+    {
+      get { return _innerArray[i]; }
+    }
+  }
+}
---
+++ 2024/c-sharp/Solutions/Solutions/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Core;
+using System.Security.Cryptography;
+void P1(string[] lines)
+{
+  MySortedList<int> left = new(lines.Length);
+  MySortedList<int> right = new(lines.Length);
+
+  for (int i = 0; i < lines.Length; i++)
+  {
+    var split = lines[i].Split("  ");
+    left.Add(Convert.ToInt32(split[0]));
+    right.Add(Convert.ToInt32(split[1]));
+  }
+
+  int sum = 0;
+  for (int i = 0; i < left.Count; i++)
+  {
+    sum += Math.Abs(left[i] - right[i]);
+  }
+  Console.WriteLine($"Result is: {sum}");
+
+}
+
+bool test = true;
+if (args.Length > 0)
+  if (args[0] == "Real")
+    test = false;
+
+string filename = test ? "input_sample.txt" : "input.txt";
+
+string[] lines = File.ReadAllLines(filename);
+P1(lines);
