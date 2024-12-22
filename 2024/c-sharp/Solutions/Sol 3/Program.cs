6cd4813cd96011b50b8eb69e1d05ebd1ed6892a9
WickedyWick <crvenialeksa@gmail.com>
2024-12-22 14:39:10 +0100

    Day 4 P2

--- 2024/c-sharp/Solutions/Sol 3/Program.cs
+++ 2024/c-sharp/Solutions/Sol 3/Program.cs
@@ -1,4 +1,6 @@
-﻿bool test = false;
+﻿using System;
+
+bool test = false;
 if (args.Length > 0)
   if (args[0] == "Real")
     test = false;
@@ -48,6 +50,39 @@ void AdvancePos(ref int currentPosition)
   if (++currentPosition == len) throw new Exception("EOF");
 }
 
+void CheckIfEnabler(ReadOnlySpan<char> buffer, ref int currPosition, ref bool enabled)
+{
+  AdvancePos(ref currPosition);
+  if (buffer[currPosition] != 'o') return;
+  AdvancePos(ref currPosition);
+  if (buffer[currPosition] == '(')
+    CheckIfDo(buffer, ref currPosition, ref enabled);
+  else if (buffer[currPosition] == 'n')
+    CheckIfDont(buffer, ref currPosition, ref enabled);
+  else
+    return;  
+}
+
+void CheckIfDo(ReadOnlySpan<char> buffer, ref int currPosition, ref bool enabled)
+{
+  AdvancePos(ref currPosition);
+  if (buffer[currPosition] != ')') return;
+  enabled = true;
+}
+
+void CheckIfDont(ReadOnlySpan<char> buffer, ref int currPosition, ref bool enabled)
+{
+  AdvancePos(ref currPosition);
+  if (buffer[currPosition] != '\'') return;
+  AdvancePos(ref currPosition);
+  if (buffer[currPosition] != 't') return;
+  AdvancePos(ref currPosition);
+  if (buffer[currPosition] != '(') return;
+  AdvancePos(ref currPosition);
+  if (buffer[currPosition] != ')') return;
+
+  enabled = false;
+}
 
 void P1(string mem)
 {
@@ -72,5 +107,28 @@ void P1(string mem)
 }
 void P2(string mem)
 {
+  if (test)
+  {
+    mem = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))\r\n";
+    len = mem.Length;
+  }
+  ReadOnlySpan<char> buffer = mem.AsSpan();
+  int sum = 0;
+  bool enabled = true;
+  for (int i = 0; i < buffer.Length; i++)
+  {
+    try
+    {
+      if (buffer[i] == 'm' && enabled)
+        CheckIfMul(buffer, ref i, ref sum);
+      else if (buffer[i] == 'd')
+        CheckIfEnabler(buffer, ref i, ref enabled);
+    }
+    catch
+    {
+
+    }
+  }
+  Console.WriteLine($"Result is {sum}");
 }
 
