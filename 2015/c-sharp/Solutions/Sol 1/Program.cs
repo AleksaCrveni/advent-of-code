cd8df1b9af808ca25c7cc34a3f9931296d80755f
WickedyWick <crvenialeksa@gmail.com>
2025-11-25 00:17:06 +0100

    2015 solutions added, Day1

---
+++ 2015/c-sharp/Solutions/Sol 1/Program.cs
@@ -0,0 +1,36 @@
+﻿bool sample = false;
+string filename = "input.txt";
+if (sample)
+  filename = "input_sample.txt";
+
+string input = File.ReadAllText(filename);
+
+Part1(input);
+Part2(input);
+void Part1(string input)
+{
+  int floor = 0;
+  foreach (char c in input)
+    if (c == '(')
+      floor++;
+    else if (c == ')')
+      floor--;
+  Console.WriteLine($"Part 1 result is {floor}");
+}
+
+void Part2(string input)
+{
+  int floor = 0;
+  int i = 0;
+  for (; i < input.Length; i++)
+  {
+    if (input[i] == '(')
+      floor++;
+    else if (input[i] == ')')
+      floor--;
+    if (floor == -1)
+      break;
+  }
+
+  Console.WriteLine($"Part 2 result is {i + 1}");
+}
