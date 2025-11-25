e0d78333fa77781b9412bdfa92b6b307e1937dac
WickedyWick <crvenialeksa@gmail.com>
2025-11-25 20:24:42 +0100

    Days 6 7 for 2015 csharp

---
+++ 2015/c-sharp/Solutions/Sol 6/Program.cs
@@ -0,0 +1,124 @@
+﻿bool sample = false;
+string filename = "input.txt";
+if (sample)
+  filename = "input_sample.txt";
+
+string[] input = File.ReadAllLines(filename);
+
+Part1(input);
+Part2(input);
+void Part1(string[] input)
+{
+  Command cmd = new Command();
+  int[,] matrix = new int[1000, 1000];
+ // input = new string[] { "turn on 0,0 through 999,999" };
+  foreach (string line in input)
+  {
+    ParseNextCommand(line, ref cmd);
+    for (int x = cmd.StartPos.x; x <= cmd.EndPos.x; x++)
+    {
+      for (int y = cmd.StartPos.y; y <= cmd.EndPos.y; y++)
+      {
+        if (cmd.CmdType == CmdType.TOGGLE)
+        {
+          matrix[x, y] = matrix[x, y] == 0 ? 1 : 0;
+        } else
+        {
+          matrix[x, y] = cmd.CmdType == CmdType.TURN_ON ? 1 : 0;
+        }
+      }
+    }
+  }
+
+  int sum = 0;
+  for (int i =0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      sum += matrix[i, j];
+    }
+  }
+  Console.WriteLine($"Part One result is {sum}");
+}
+
+
+
+void Part2(string[] input)
+{
+  Command cmd = new Command();
+  int[,] matrix = new int[1000, 1000];
+  // input = new string[] { "turn on 0,0 through 999,999" };
+  foreach (string line in input)
+  {
+    ParseNextCommand(line, ref cmd);
+    for (int x = cmd.StartPos.x; x <= cmd.EndPos.x; x++)
+    {
+      for (int y = cmd.StartPos.y; y <= cmd.EndPos.y; y++)
+      {
+        if (cmd.CmdType == CmdType.TOGGLE)
+        {
+          matrix[x, y] += 2;
+        }
+        else
+        {
+          if (cmd.CmdType == CmdType.TURN_ON)
+            matrix[x, y] += 1;
+          else
+          {
+            matrix[x, y] -= matrix[x, y] == 0 ? 0 : 1;
+          }
+        }
+      }
+    }
+  }
+
+  int sum = 0;
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      sum += matrix[i, j];
+    }
+  }
+  Console.WriteLine($"Part 2 result is {sum}");
+}
+
+void ParseNextCommand(string line, ref Command cmd)
+{
+  string[] split = line.Split(' ');
+  int pos = 0;
+  if (split[pos++] == "toggle")
+  {
+    cmd.CmdType = CmdType.TOGGLE;
+  } else
+  {
+    if (split[pos++] == "off")
+      cmd.CmdType = CmdType.TURN_OFF;
+    else
+      cmd.CmdType = CmdType.TURN_ON;
+  }
+  string[] nums = split[pos++].Split(',');
+  (int x, int y) coord = (Convert.ToInt32(nums[0]), Convert.ToInt32(nums[1]));
+  pos++;
+  cmd.StartPos = coord;
+  nums = split[pos++].Split(',');
+  coord = (Convert.ToInt32(nums[0]), Convert.ToInt32(nums[1]));
+  cmd.EndPos = coord;
+}
+
+
+struct Command
+{
+  public CmdType CmdType;
+  public (int x, int y) StartPos;
+  public (int x, int y) EndPos;
+}
+
+
+enum CmdType
+{
+  TOGGLE,
+  TURN_OFF,
+  TURN_ON
+}
+
---
+++ 2015/c-sharp/Solutions/Sol 7/Program.cs
@@ -0,0 +1,147 @@
+﻿using System.Diagnostics;
+
+bool sample = false;
+string filename = "input.txt";
+if (sample)
+  filename = "input_sample.txt";
+
+string[] input = File.ReadAllLines(filename);
+
+Part1(input);
+Part2(input);
+void Part1(string[] input)
+{
+  Dictionary<string, Command> commands = new Dictionary<string, Command>();
+  Command cmd = new Command();
+  foreach (string line in input)
+  {
+    ParseCommand(line, ref cmd);
+    commands.Add(cmd.Dest, cmd);
+  }
+  HashSet<string> lookupList = new();
+  ushort res = GetFinalVal("a", commands, lookupList);
+  Console.WriteLine($"Part one solution is {res}");
+}
+
+void Part2(string[] input)
+{
+  Dictionary<string, Command> commands = new Dictionary<string, Command>();
+  Command cmd = new Command();
+  foreach (string line in input)
+  {
+    ParseCommand(line, ref cmd);
+    commands.Add(cmd.Dest, cmd);
+  }
+  HashSet<string> lookupList = new();
+  ushort res = GetFinalVal("a", commands, lookupList);
+  // I REALLY SHOULD HAVE JUST USED CLASS
+
+  cmd = commands["b"];
+  cmd.Op = Operation.SET;
+  cmd.LeftOperand = res.ToString();
+  commands["b"] = cmd;
+  foreach (KeyValuePair<string, Command> kvp in commands)
+  {
+    Command c = kvp.Value;
+    c.Computed = null;
+    commands[kvp.Key] = c;
+  }
+  lookupList.Clear();
+  res = GetFinalVal("a", commands, lookupList);
+  Console.WriteLine($"Part two solution is {res}");
+}
+
+
+ushort GetFinalVal(string key, Dictionary<string, Command> commands, HashSet<string> lookupList )
+{
+  bool exists = lookupList.Add(key);
+  
+  // special case for AND wheere 1 AND can happen
+  if (char.IsDigit(key[0]))
+    return Convert.ToUInt16(key);
+  
+  // WRITE DEBUG LOOKUP TABLE
+  Command cmd = commands[key];
+  if (cmd.Computed != null)
+    return (ushort)cmd.Computed;
+  Debug.Assert(exists);
+  ushort res = 0;
+  switch (cmd.Op)
+  {
+    case Operation.SET:
+      res = Convert.ToUInt16(cmd.LeftOperand);
+      break;
+    case Operation.COPY:
+      res = GetFinalVal(cmd.LeftOperand, commands, lookupList);
+      break;
+    case Operation.NOT:
+      res = (ushort)(~GetFinalVal(cmd.RightOperand, commands, lookupList));
+      break;
+    case Operation.AND:
+      res = (ushort)(GetFinalVal(cmd.LeftOperand, commands, lookupList) & GetFinalVal(cmd.RightOperand, commands, lookupList));
+      break;
+    case Operation.OR:
+      res = (ushort)(GetFinalVal(cmd.LeftOperand, commands, lookupList) | GetFinalVal(cmd.RightOperand, commands, lookupList));
+      break;
+    case Operation.LSHIFT:
+      res = (ushort)(GetFinalVal(cmd.LeftOperand, commands, lookupList) << Convert.ToUInt16(cmd.RightOperand));
+      break;
+    case Operation.RSHIFT:
+      res = (ushort)(GetFinalVal(cmd.LeftOperand, commands, lookupList) >> Convert.ToUInt16(cmd.RightOperand));
+      break;
+    default:
+      throw new Exception("WTF");
+  }
+  cmd.Computed = res;
+  commands[key] = cmd;
+  return (ushort)cmd.Computed;
+}
+
+void ParseCommand(string line, ref Command cmd)
+{
+  string[] split = line.Split(' ');
+  if (split.Length == 3)// either SET or COPY
+  {
+    if (char.IsDigit(split[0][0]))
+      cmd.Op = Operation.SET;
+    else
+      cmd.Op = Operation.COPY;
+
+    cmd.LeftOperand = split[0];
+    cmd.Dest = split[2];
+    return;
+  }
+
+  if (split.Length == 4)
+  {
+    cmd.Op = Operation.NOT;
+    cmd.RightOperand = split[1];
+    cmd.Dest = split[3];
+    return;
+  }
+
+  cmd.Op = Enum.Parse<Operation>(split[1]);
+  cmd.LeftOperand = split[0];
+  cmd.RightOperand = split[2];
+  cmd.Dest = split[4];
+}
+struct Command
+{
+  public Command() { }
+  public Operation Op;
+  public string LeftOperand;
+  public string RightOperand;
+  public string Dest;
+  public ushort? Computed;
+}
+
+enum Operation
+{
+  SET, // left is number
+  COPY, // left is another dest
+  AND,
+  OR,
+  LSHIFT,
+  RSHIFT,
+  NOT // right is value
+}
